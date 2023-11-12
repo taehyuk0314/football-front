945c671844e4156999eb74b83609bfd4f1bdc2e1
taehyek0314 <taehyek0314@gmail.com>
2023-11-12 17:00:20 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from "./components/Header";
+
+export default class App extends React.Component  {
+  render() {
+    return (
+      <>
+      <div className="App">
+        <Header/>
+      </div>      
+      </>
+    )
+  }
+ 
+}
